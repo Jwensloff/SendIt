4f979e1df8d2d9e95fb4fc29a4281b7dadbe6931
Jocelyn Wensloff <jewensloff@gmail.com>
2024-06-25 11:50:15 -0600

    Add countdown component to Home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./HomePage.scss";
 import { Link } from "react-router-dom";
 
@@ -25,6 +25,34 @@ const HomePage = () => {
       link: "favorites",
     },
   ];
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const target = new Date("12/21/2024 23:59:59");
+
+    const interval = setInterval(() => {
+      const now = new Date();
+      const difference = target.getTime() - now.getTime();
+
+      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+      setDays(d);
+
+      const h = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      setHours(h);
+
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      setMinutes(m);
+
+      const s = Math.floor((difference % (1000 * 60)) / 1000);
+      setSeconds(s);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   const cards = landingPageData.map((item, index) => {
     return (
@@ -38,7 +66,23 @@ const HomePage = () => {
   return (
     <section className="home-page">
       <div className="background-image">
-        <p>Countdown to Winter:</p>
+        <div className="countdown-container">
+          <p>Countdown to Winter:</p>
+          <div className="timer-wrapper">
+            <div>
+              <span>{days}</span> <span>Days</span>
+            </div>
+            <div>
+              <span>{hours}</span> <span>Hours</span>
+            </div>
+            <div>
+              <span>{minutes}</span> <span>Minutes</span>
+            </div>
+            <div>
+              <span>{seconds}</span> <span>Seconds</span>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="home-content">
         <div className="home-page-card-container">{cards}</div>
